edb3e5351b729a896183a99d73facb85878b5865
skiedflakes <46420286+skiedflakes@users.noreply.github.com>
2019-10-23 11:34:41 +0800

    Update frag_Home_main.java

--- app/src/main/java/com/wdysolutions/notes/Home/frag_Home_main.java
+++ app/src/main/java/com/wdysolutions/notes/Home/frag_Home_main.java
@@ -31,7 +31,7 @@ public class frag_Home_main extends Fragment {
     String selectedNOTES="";
     SharedPref sharedPref;
 
-    //aron bitch
+    //test aron
 
     private void initMenu(View view){
         final Toolbar toolbar = view.findViewById(R.id.toolbar);
