3ad040547e8f7f2d26d0d01188fee809f41cc9ba
Ron <thechosenweak@gmail.com>
2019-11-04 14:16:21 +0800

    Merge branch 'debugging' into aron_debug
    # Conflicts:
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/wdysolutions/notes/Notes_Pig/Parity_Report/Parity_report_main.java
@@ -0,0 +1,27 @@
+package com.wdysolutions.notes.Notes_Pig.Parity_Report;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wdysolutions.notes.R;
+
+
+public class Parity_report_main extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.parity_report_main, container, false);
+
+
+
+        return view;
+    }
+
+
+}
